f67e08b33a05117538ab20c603c3ab7ea48790b1
raegertay <raegertay89@gmail.com>
2017-11-29 15:48:09 +0800

    Added controller specs for pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,4 +2,22 @@ require 'rails_helper'
 
 RSpec.describe PagesController, type: :controller do
 
+  let(:user) { create(:user) }
+  before { sign_in user }
+
+  describe 'GET #dashboard' do
+
+    let(:note) { create(:note, user: user) }
+    let(:user2) { create(:user) }
+
+    before do
+      get :dashboard
+    end
+
+    it { expect(assigns(:note)).to be_a_new(Note) }
+    it { expect(assigns(:notes)).to contain_exactly(note) }
+    it { expect(assigns(:users)).to contain_exactly(user2) }
+
+  end
+
 end
--- spec/factories/notes.rb
+++ spec/factories/notes.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :note do
-    
+    association :user, factory: :user
+    title 'Title'
+    body 'Body'
   end
 end
