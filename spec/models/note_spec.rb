4d90d9ad7463783e7a2399750afb78780bad7946
raegertay <raegertay89@gmail.com>
2017-11-29 16:24:45 +0800

    Added specs for note model methods

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -5,4 +5,17 @@ RSpec.describe Note, type: :model do
   it { is_expected.to validate_presence_of(:title) }
   it { is_expected.to have_many(:likes).dependent(:destroy) }
 
+  let(:user) { create(:user) }
+  let(:note) { create(:note, user: user) }
+  let(:user2) { create(:user) }
+  before { note.likes.create(user: user2) }
+
+  describe '#liked?' do
+    it { expect(note.liked?(user2)).to eq(true) }
+  end
+
+  describe '#total_likes' do
+    it { expect(note.total_likes).to eq(1) }
+  end
+
 end
