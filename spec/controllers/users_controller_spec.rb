c4488e79ba26b24c73f2ebc5ee44d93951a26af0
raegertay <raegertay89@gmail.com>
2017-11-29 15:37:10 +0800

    Added controller specs for users

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,6 +2,24 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
-  
+  let(:user) { create(:user) }
+  before { sign_in user }
+
+  describe 'POST #follow' do
+    let(:user2) { create(:user) }
+    before { post :follow, params: { id: user2.id } }
+    it { expect(user.followees.count).to eq(1) }
+    it { is_expected.to redirect_to(root_path) }
+  end
+
+  describe 'DELETE #unfollow' do
+    let(:user2) { create(:user) }
+    before do
+      user.follow(user2)
+      delete :unfollow, params: { id: user2.id }
+    end
+    it { expect(user.followees.count).to eq(0) }
+    it { is_expected.to redirect_to(root_path) }
+  end
 
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :user do
-    
+    sequence(:email) { |n| "test#{n}@email.com" }
+    password 'password'
   end
 end
