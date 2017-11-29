86907aa0308268956d8e40f35d404155506a6d0a
raegertay <raegertay89@gmail.com>
2017-11-29 15:27:58 +0800

    Added model specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it { is_expected.to have_many(:notes).dependent(:destroy) }
+  it { is_expected.to have_many(:followings).with_foreign_key(:follower_id).dependent(:destroy) }
+  it { is_expected.to have_many(:followees).through(:followings) }
+  it { is_expected.to have_many(:inverse_followings).class_name('Following').with_foreign_key(:followee_id).dependent(:destroy) }
+  it { is_expected.to have_many(:followers).through(:inverse_followings) }
+  it { is_expected.to have_many(:likes).dependent(:destroy) }
+
 end
